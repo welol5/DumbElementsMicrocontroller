7f87964a3d18aa033fd93e5bf012cdbaacf64834
William Elliman <william.elliman@gmail.com>
2023-12-05 20:50:56 -0600

    Added basic animation engine

---
+++ RaspberryPi/AnimationEngine.py
@@ -0,0 +1,58 @@
+import colorsys
+import random
+import threading
+import time
+
+class AnimationEngine(threading.Thread):
+
+    stop = 0
+    animation = None
+    leds = None
+    ledCount = None
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        if (self.animation != None):
+            self.animation(self.leds, self.ledCount)
+
+    def stop(self):
+        self._stop_event.set()
+
+    def setAnimation(self, name, leds, ledCount):
+        if(name == "stars"):
+            self.animation = self.stars
+            self.leds = leds
+            self.ledCount = ledCount
+
+    def stars(self, leds, ledCount):
+        chance = 0.005
+        colors = [(0,0,0)]*ledCount
+        leds_on = [0]*ledCount
+        print(leds[0])
+
+        stop = 0
+        while stop == 0:
+            time.sleep(0.1)
+            #set colors
+            for i in range(ledCount):
+                leds[i] = colors[i]
+                
+                #add new stars
+                if leds_on[i] == 0:
+                    if random.random() < chance:
+                        leds_on[i] = 1
+                        colors[i] = (random.random()*256,random.random()*128,random.random()*256)
+
+                #fade old stars
+                else:
+                    hls = colorsys.rgb_to_hls(colors[i][0],colors[i][1],colors[i][2])
+                    new_hls = (hls[0], hls[1]-1, hls[2])
+                    if new_hls[1] <= 0:
+                        colors[i] = (0,0,0)
+                        leds_on[i] = 0
+                    else:
+                        colors[i] = colorsys.hls_to_rgb(new_hls[0],new_hls[1],new_hls[2])
+            leds.show()
+                
--- RaspberryPi/Server.py
+++ RaspberryPi/Server.py
@@ -3,30 +3,47 @@ import time
 import json
 import board
 import neopixel
+import threading
 
-host = "localhost"
-port = 8080
+from AnimationEngine import AnimationEngine
+
+host = "0.0.0.0"
+port = 8081
+
+ledCount = 450
+leds = neopixel.NeoPixel(board.D18, ledCount, auto_write=False)
+leds.fill((0, 0, 0))
+leds.show()
+animationThread = None
 
 class LEDServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes("""{"ledCount":"450", "ledAddressible":"true", "ledAnimations":"true"}""", "utf-8"))
 
     def do_POST(self):
+        if(self.path == '/led'):
+            contentLength = int(self.headers.get('Content-Length'))
+            body = json.loads(self.rfile.read(contentLength))
+            print(json.dumps(body, indent=4))
+            self.update_leds(body)
+        elif(self.path == '/led/namedanimation'):
+            animationThread = AnimationEngine()
+            animationThread.setAnimation("stars", leds, ledCount)
+            animationThread.start()
+
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes("""{"ledCount":"450", "ledAddressible":"true", "ledAnimations":"true"}""", "utf-8"))
+
+    def update_leds(self, command):
+        for status in command["status"]:
+            for i in range(status["ledStart"], status["ledEnd"]):
+                leds[i] = (status["r"],status["g"],status["b"])
+        leds.show()
+        print(command)
 
 if __name__ == "__main__":
     webServer = HTTPServer((host, port), LEDServer)
@@ -40,3 +57,84 @@ if __name__ == "__main__":
 
     webServer.server_close()
     print("server stopped")
+
+
+
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# # Simple test for NeoPixels on Raspberry Pi
+# import time
+# import board
+# import neopixel
+
+
+# # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# # NeoPixels must be connected to D10, D12, D18 or D21 to work.
+# pixel_pin = board.D18
+
+# # The number of NeoPixels
+# num_pixels = 2
+
+# # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+# ORDER = neopixel.GRB
+
+# pixels = neopixel.NeoPixel(
+#     pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=ORDER
+# )
+
+# def wheel(pos):
+#     # Input a value 0 to 255 to get a color value.
+#     # The colours are a transition r - g - b - back to r.
+#     if pos < 0 or pos > 255:
+#         r = g = b = 0
+#     elif pos < 85:
+#         r = int(pos * 3)
+#         g = int(255 - pos * 3)
+#         b = 0
+#     elif pos < 170:
+#         pos -= 85
+#         r = int(255 - pos * 3)
+#         g = 0
+#         b = int(pos * 3)
+#     else:
+#         pos -= 170
+#         r = 0
+#         g = int(pos * 3)
+#         b = int(255 - pos * 3)
+#     return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+# def rainbow_cycle(wait):
+#     for j in range(255):
+#         for i in range(num_pixels):
+#             pixel_index = (i * 256 // num_pixels) + j
+#             pixels[i] = wheel(pixel_index & 255)
+#         pixels.show()
+#         time.sleep(wait)
+
+
+# while True:
+#     # Comment this line out if you have RGBW/GRBW NeoPixels
+#     pixels.fill((255, 0, 0))
+#     # Uncomment this line if you have RGBW/GRBW NeoPixels
+#     # pixels.fill((255, 0, 0, 0))
+#     pixels.show()
+#     time.sleep(1)
+
+#     # Comment this line out if you have RGBW/GRBW NeoPixels
+#     pixels.fill((0, 255, 0))
+#     # Uncomment this line if you have RGBW/GRBW NeoPixels
+#     # pixels.fill((0, 255, 0, 0))
+#     pixels.show()
+#     time.sleep(1)
+
+#     # Comment this line out if you have RGBW/GRBW NeoPixels
+#     pixels.fill((0, 0, 255))
+#     # Uncomment this line if you have RGBW/GRBW NeoPixels
+#     # pixels.fill((0, 0, 255, 0))
+#     pixels.show()
+#     time.sleep(1)
+
+#     rainbow_cycle(0.1)  # rainbow cycle with 1ms delay per step
